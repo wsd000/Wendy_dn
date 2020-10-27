4f095ef55f8bd4da644a0a22ad8d7d3cdde051af
wsd <1411565873@qq.com>
2020-10-27 15:26:12 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+    title: 'wendy日常录',  // 设置网站标题
+    keywords: '前端开发',
+    description : '日常笔记',
+    // repo: 'https://github.com/zeroonbush/blog.git',  //仓库地址
+    // base: '/blog/',  // 配合部署项目
+    head: [
+        ['link', { rel: 'shortcut icon', type: "image/x-icon", href: '/favicon.png' }]
+    ],
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@vuepress': path.join(__dirname, './public/img'),
+            }
+        }
+    },
+    themeConfig: {  //主题配置
+        logo: '/img/logo.png',
+        nav: [  //导航栏
+            { text: 'Home', link: '/' },
+            { text: 'Notes', link: '/notes_docs/' },
+            { text: 'Framework', link: '/framework_docs/' },
+            { text: 'Wheel', link: '/wheel_docs/' },
+            { text: 'Tool', link: '/tool_docs/' },
+            { text: 'KL_Timer', link: '/KL_docs/'},
+            { text: 'github', link: 'https://github.com/zeroonbush/blog.git' }
+        ],
+        sidebar: {  //侧边拦
+            '/notes_docs/': [
+                {
+                    title: '随记', //侧边栏名称
+                    collapsable: false, // 是否可折叠
+                    children: [
+                        '',
+                        'css',
+                        'js',
+                        'applet'
+                    ]
+                  }
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true 
+heroImage: /img/webp.png
+actionText: Recording →
+actionLink: /zh/guide/
+features:
+- title: 疑难杂症
+  details: 症结描述：填坑引领的血泪记录、不常用但好用的编写技巧、易混淆难理解的经典金题。（绝对经典！！！）
+- title: 七拼八凑
+  details: 拼经验，凑精华！整理，编写了工作中的常用组件：取长补短、灵活出口、框架兼容。（不敢言说强大，仅是便利工作，提升效率，欢迎鉴讨！）
+- title: oh！新大陆
+  details: 与时俱进，科普最新前沿技术，定期更新，紧跟变革！（争取下一个记名名额~~~）
+footer: Private Licensed | Copyright © 2017-present Evan You
+---
